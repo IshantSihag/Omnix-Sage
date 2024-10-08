4aa831d634cabe586fbb29356f034fa28761edda
Anmol Verma <anmolverma@Anmols-MacBook-Air.local>
2024-10-08 22:01:08 +0530

    rule book

--- frontend/src/RuleBook.jsx
+++ frontend/src/RuleBook.jsx
@@ -7,11 +7,24 @@ function RuleBook() {
   return (
     <div>
     <Header />
-      <h1>Rule Book</h1>
       <p>Here are the rules to follow when investing in the stock market:</p>
       <ul>
-        <li>Rule 1: Don't lose money.</li>
-        <li>Rule 2: Don't forget Rule 1.</li>
+        <li>Invest for long term</li>
+            <ul>Buy businesses ypou believe can grow steadily over 10+ years</ul>
+        <li>Focus on Quality Businesses</li>
+            <ul>Businesses with High ROE , Strong Cash Flow and consistent Earinngs with low debt</ul>
+        <li>Buy at a reasonable price</li>
+            <ul>Buy when the stock is trading at P/E less than PAT CAGR</ul>
+        <li>Be contrarian and think independently </li>
+            <ul>Based your decision on sound analysis</ul>
+        <li>Understand what you invest in</li>
+            <ul>Learn how they operate , generate profit and grow</ul>
+        <li>Risk management is key</li>
+            <ul>Consider potential risk and ensure they are managable</ul>
+        <li>Keep it simple</li>
+            <ul>Avoid speculation</ul>
+        
+          
       </ul>
     <Footer />
     </div>
